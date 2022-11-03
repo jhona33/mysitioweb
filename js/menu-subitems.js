2fb55a541c8bd257438ef1ff5a996afe9b831219
kevinamunoz <kevinamunoz11@gmail.com>
2022-11-03 11:28:23 -0500

    agregar menujs

---
+++ js/menu-subitems.js
@@ -0,0 +1,13 @@
+
+let botonMenujs = document.getElementById("botonInicio");
+
+botonMenujs.addEventListener("click", function(){
+    alert("primer linea js");
+});
+
+let botonMenu = document.getElementById("botonContac");
+
+botonMenu.addEventListener("click", function(){
+    alert("nuestras ofertas");
+});
+
